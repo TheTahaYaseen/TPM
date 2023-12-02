94454f6975c5156c4417c9ce821d457eebc54e9b
The Taha Yaseen <thetahayaseen@gmail.com>
2023-12-03 01:07:33 +0500

    Basic Settings

---
+++ pseudocode.py
@@ -0,0 +1,13 @@
+
+# The Project Manager
+
+"""
+
+The Ultimate Project Manager
+- I Could Have Used A Kanban Board But Here I Am
+
+# v0.1
+    With Basic Auth; Just A Basic CRUD For Project
+    Project Details To Consider: Name, Any Abbreviation, Description, Purpose, Pseudocode/MindMapping & State/Progress.
+
+"""
